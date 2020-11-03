506d08dd6f67b49199c51fdc29d33bbdd9dc4a02
igogo <chia0210@gmail.com>
2020-11-03 15:39:47 +0800

    not manager account will be redirect from admin page

--- src/main/java/app/passwd/controller/admin/AdminController.java
+++ src/main/java/app/passwd/controller/admin/AdminController.java
@@ -1,14 +1,11 @@
 package app.passwd.controller.admin;
 
 import app.passwd.repository.LdapRepository;
-import app.passwd.repository.SystemConfigRepository;
-import app.passwd.service.Oauth2Client;
 import app.passwd.service.UserLoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -18,21 +15,16 @@ public class AdminController {
     @Autowired
     UserLoginService userloginservice;
 
-//    @Autowired
-//    Oauth2Client client;
-//
-
-//
-//    @Autowired
-//    SystemConfigRepository sysconfigrepository;
-//
-//    @Autowired
-//    LdapRepository ldaprepository;
+    @Autowired
+    LdapRepository ldapRepository;
 
 
     @GetMapping("/passwd/admin")
     public String userhome() {
-        if (!userloginservice.isLoggedin()) {
+        logger.info(userloginservice.getUser().getName());
+        if (!userloginservice.isLoggedin() || !userloginservice.getUser().getUsername().equals(ldapRepository.findBySn(1).getAccountManager())) {
+            logger.info("logging user: " + userloginservice.getUser());
+            logger.info("account manager: " + ldapRepository.findBySn(1).getAccountManager());
             return "redirect:/";
         }
 
@@ -42,9 +34,7 @@ public class AdminController {
 
     @GetMapping("/admin")
     public String userhomeProxypass() {
-//        if (!userloginservice.isLoggedin()) {
-//            return "redirect:/";
-//        }
+
         return userhome();
     }
 
